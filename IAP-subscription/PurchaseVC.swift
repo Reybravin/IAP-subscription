26bf0fa455b469763c9d67f3712de4bd5c1ecc99
ss <reybravin@gmail.com>
2018-02-11 15:41:52 +0200

    minor changes

--- IAP-subscription/PurchaseVC.swift
+++ IAP-subscription/PurchaseVC.swift
@@ -14,6 +14,8 @@ class PurchaseVC: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //some function
 
 }
 
