2c35addf080782c6f15e6470465fbd8fc1099ea1
ss <reybravin@gmail.com>
2018-02-11 15:34:51 +0200

    Initial commit

---
+++ IAP-subscription/PurchaseVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IAP-subscription
+//
+//  Created by Serhii on 2/11/18.
+//  Copyright © 2018 Serhii. All rights reserved.
+//
+
+import UIKit
+
+class PurchaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
